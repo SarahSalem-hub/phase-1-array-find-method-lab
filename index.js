676cba2dc5a3313860ae3b49119def74d67adda3
Sarah Al-Dhaferi <55980263+SarahSalem-hub@users.noreply.github.com>
2022-12-26 19:02:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "d"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+
+function superbowlWin(array){
+    
+   let foundedObj = array.find(arrayfind)
+
+    if ( foundedObj !== undefined)
+    {
+        return foundedObj['year']
+    }
+    else {
+        return undefined
+    }
+
+
+}
+function arrayfind(element){
+    
+    if(element['result'] === 'W'){
+        return element
+    }
+    
+}
+console.log(superbowlWin(record))
