74057ab07426571fb454398d6a6eb4ba310641e0
Sarah Al-Dhaferi <55980263+SarahSalem-hub@users.noreply.github.com>
2022-12-26 21:44:56 +0300

    superbowlWin refactored

--- index.js
+++ index.js
@@ -2,30 +2,25 @@
 
 const record = [
     {year: "2018", result: "L"},
-    {year: "2017", result: "d"},
+    {year: "2017", result: "W"},
     {year: "2016", result: "N/A"}
     //...
   ]
 
 function superbowlWin(array){
     
-   let foundedObj = array.find(arrayfind)
-
-    if ( foundedObj !== undefined)
-    {
-        return foundedObj['year']
-    }
-    else {
+    let res = array.find(function(element){
+        return element['result'] === 'W'
+        
+    })
+    if(res){
+        return res.year
+    }else{
         return undefined
     }
 
+    
 
-}
-function arrayfind(element){
-    
-    if(element['result'] === 'W'){
-        return element
-    }
-    
+
 }
 console.log(superbowlWin(record))
